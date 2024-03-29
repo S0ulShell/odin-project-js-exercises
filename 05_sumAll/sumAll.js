3bf2723095ac302bc9b73d94aee826a296918d69
S0ulShell <rajatpkumar24@gmail.com>
2024-03-30 00:22:16 +1300

    finshed exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    
+    let answer  = 0;
+    let error = "ERROR";
 
+    if ((int1 > 0 && int2 > 0) && (Number.isInteger(int1) && Number.isInteger(int2))){
+        if(int1 > int2){
+            for(int2; int2 <= int1; int2++ )
+                answer = answer + int2;
+        }else{
+            for(int1; int1 <= int2; int1++ )
+                answer = answer + int1;
+        }
+    }else{
+        return error;
+    } 
+
+    return answer;
 };
 
 // Do not edit below this line

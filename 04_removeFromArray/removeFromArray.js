e8f8112ff795032b54389e6f419fb7b790f4fc19
S0ulShell <rajatpkumar24@gmail.com>
2024-03-29 23:59:22 +1300

    finshed exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...nums) {
 
+    var i = 0;
+
+    for(let value in nums){
+        array = array.filter(a => a !== nums[value]);
+        // while (i < array.length){
+        //     if(array[i] === nums[value]){
+        //         array.splice(i, 1);
+        //         ++i;
+        //     }else{
+        //         ++i;
+        //     }
+        // } 
+    }
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;

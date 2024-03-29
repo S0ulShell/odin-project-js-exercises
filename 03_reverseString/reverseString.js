2d67c9c40c7fc603fd5e5113d32bfa5d6dd20a23
S0ulShell <rajatpkumar24@gmail.com>
2024-03-29 22:11:34 +1300

    finished exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    let answer = '';
+    let length = string.length;
+    let counter = string.length;
 
+    if(length > 0){
+        for(let i = 0; i < counter; i++){
+            answer = answer.concat(string.at(length -1 ));
+            length = length - 1;
+        }
+    }else{
+        return answer;
+    }
+
+    return answer;
 };
 
 // Do not edit below this line

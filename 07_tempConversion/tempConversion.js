3d335e3aec13973aafa3c9675fa2b457cc7de063
S0ulShell <rajatpkumar24@gmail.com>
2024-03-30 01:37:53 +1300

    finished exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = 0;
+  celsius = (temp - 32) * (5/9);
+  celsius = +celsius.toFixed(1)
+  // if(celsius === "0.0"){
+  //   celsius = 0;
+  // }
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = 0;
+  fahrenheit = temp * (9/5) + 32;
+  fahrenheit = +fahrenheit.toFixed(1)
+  // if(fahrenheit === "0.0"){
+  //   fahrenheit = 0;
+  // }
+  return fahrenheit;
 };
 
 // Do not edit below this line

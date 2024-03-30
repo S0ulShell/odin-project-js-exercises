2ef2b55a2ad0848aef75d23991a268e73aa0e0c5
S0ulShell <rajatpkumar24@gmail.com>
2024-03-30 17:21:52 +1300

    finished exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(int) {
+    
+    let array = [1, 1];
 
+    let index = parseInt(int);
+
+    let counter = parseInt(int);
+
+    if(counter < 0){
+        return("OOPS");
+    }
+    if(counter == 0){ 
+        return 0;
+    }
+    while(counter > 2){
+        array.push((array[array.length -1]) + (array[array.length - 2]))
+        --counter;
+    }
+    answer = array[index - 1];
+    return answer;
 };
 
 // Do not edit below this line

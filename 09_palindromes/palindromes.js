a9c32bced626b5a99ef9edd34579c28fe63b966b
S0ulShell <rajatpkumar24@gmail.com>
2024-03-30 17:00:12 +1300

    fnished exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let answer= "";
+    let simple = string.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+    let counter = simple.length;
 
+    while(counter > 0){
+        answer = answer.concat(simple[counter - 1])
+        --counter;
+    }
+    if (answer === simple){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
+
 module.exports = palindromes;

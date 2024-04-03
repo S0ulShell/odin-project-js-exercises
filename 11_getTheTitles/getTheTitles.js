a0be03e1b1f2153c30c3236d46cb2477a1e4ef85
S0ulShell <rajatpkumar24@gmail.com>
2024-04-03 21:09:25 +1300

    finished exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    
+    let titles = [];
 
+    array.forEach(function (arrayItem) {
+        titles.push(arrayItem.title);
+        console.log(titles);
+    });
+    return  titles;
 };
 
 // Do not edit below this line

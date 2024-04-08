1a78257dd222c1fa0ad07bcf331d911a8d22ba4d
S0ulShell <rajatpkumar24@gmail.com>
2024-04-08 14:01:56 +1200

    finshed exercise, did not use mpa, filter or reduce, as i need to learn more about them to use properly

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    let index = 0;
+    let age = 0;
+    let oldest = 0;
+    const currYear = 2024;
 
+
+    for (let i = 0; i < array.length; i++) {
+        if ('yearOfDeath' in array[i]) {
+            age = array[i].yearOfDeath - array[i].yearOfBirth
+            if (age > oldest) {
+                oldest = age;
+                index = i;
+            }
+        }else{
+            age = currYear - array[i].yearOfBirth
+            if (age > oldest) {
+                oldest = age;
+                index = i;
+            }
+        }
+
+    }
+    return array[index];
 };
 
 // Do not edit below this line
+
 module.exports = findTheOldest;
